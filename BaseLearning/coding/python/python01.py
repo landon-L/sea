3a0423f79f37de98782ba368bb027f48494c6afb
onepeoplerainyday <1412840154@qq.com>
2018-06-04 22:14:39 +0800

    python 学习

--- BaseLearning/coding/python/python01.py
+++ BaseLearning/coding/python/python01.py
@@ -27,31 +27,72 @@ args ['55', '77']
 
 """
 
-import sys, getopt
+# import sys, getopt
 
-def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv, "hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print ('test.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print ('test.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print ('输入的文件为：', inputfile)
-   print ('输出的文件为：', outputfile)
-   print("opts", opts)
-   print("args", args)
+# def main(argv):
+#    inputfile = ''
+#    outputfile = ''
+#    try:
+#       opts, args = getopt.getopt(argv, "hi:o:",["ifile=","ofile="])
+#    except getopt.GetoptError:
+#       print ('test.py -i <inputfile> -o <outputfile>')
+#       sys.exit(2)
+#    for opt, arg in opts:
+#       if opt == '-h':
+#          print ('test.py -i <inputfile> -o <outputfile>')
+#          sys.exit()
+#       elif opt in ("-i", "--ifile"):
+#          inputfile = arg
+#       elif opt in ("-o", "--ofile"):
+#          outputfile = arg
+#    print ('输入的文件为：', inputfile)
+#    print ('输出的文件为：', outputfile)
+#    print("opts", opts)
+#    print("args", args)
 
-if __name__ == "__main__":
-   main(sys.argv[1:])
+# if __name__ == "__main__":
+#    main(sys.argv[1:])
 
+
+#################################################
+lista = ["abc", 123, 2.5, 23,33,22,55]
+listb = ["abc","dd"]
+print(lista)
+print(lista[1:])
+print(lista + listb)
+
+tup2=(20)
+print(tup2)
+
+# 集合的
+classA = {"Tom","Jim","Jack","Tom"}
+classD = {"Tom"}
+classC = set("abctoa")
+classB = set("Tom")
+print("集合测试")
+print(classA & classB)
+print(classB & classC)
+print(classD & classB)
+
+print(classD | classC)  
+print(classB | classC) 
+print(classA | classC)
+
+
+# 字典类型
+dic = {}
+dic["one"] = 1
+dic[2] = 0.1
+print(dic[2])
+
+dica = {x: x**2 for x in (2, 4, 6)}
+print(dica)
+
+#函数测试
+a = 10
+def text():
+    a = a + 1
+    print(a)
+text()
 
 
